4822e1de0e7181bd182982c27460f6fa6610dc81
Malgorzata Kapuscinska <m_kapuscinska1@o2.pl>
2018-04-19 21:50:44 +0200

    Add MessageList component

---
+++ client/MessageList.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import styles from './MessageList.css';
+
+const Message = props => {
+	<div className={styles.Message}>
+		<strong>{props.from} :</strong>
+		<span>{props.text}</span>
+	</div>
+};
+
+const MessageList = props => {
+	<div className={styles.MessasgeList}>
+		{
+			props.message.map(message, i) => {
+				<Message 
+					key=i
+					from={message.from}
+					text={message.text}
+				/>
+			}
+		}
+	</div>
+};
+
+export default MessageList;
